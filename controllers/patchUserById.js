c0e12d1e828d32e39d2895ee8322b19b4da4de74
Steph-crown <emmanuelstephen024@gmail.com>
2021-01-13 18:12:22 +0100

    Finished patchUserById controller

--- controllers/patchUserById.js
+++ controllers/patchUserById.js
@@ -1,3 +1,17 @@
+const Users = require('./../models/users')
+
+
 module.exports = (req, res, next) => {
-    res.send(req.params.id + "patch")
-}
+    let id = req.params.id;
+    Users.findOneAndUpdate({id}, req.body, {new: true}, (err, data) => {
+        if (!data) {
+            res.status(503).json({
+                error: "Error updating data" + err
+            })
+        } else {
+            res.status(200).json({
+                data
+            })
+        }
+    })
+};

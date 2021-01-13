02a21dfeddc932c47507e79126726dc8391b23df
Steph-crown <emmanuelstephen024@gmail.com>
2021-01-13 17:58:24 +0100

    Finished the getUsers.js and getUserById controllers

--- controllers/getUserById.js
+++ controllers/getUserById.js
@@ -1,3 +1,16 @@
+const Users = require('./../models/users');
+
 module.exports = (req, res, next) => {
-    res.send(req.params.id);
-}
+    let id = req.params.id;
+    Users.findOne({id: id}, (err, data) => {
+        if (err) {
+            res.status(402).json({
+                error: "Data not found"
+            })
+        } else {
+            res.status(200).json({
+                data
+            })
+        }
+    })
+};
--- controllers/getUsers.js
+++ controllers/getUsers.js
@@ -1,3 +1,16 @@
+const User = require('./../models/users')
+
 module.exports = (req, res) => {
-    res.send('faag')
+    User.find((err, data) => {
+        if (err) {
+            res.status(502).json({
+                error: "Error fetching data" + err
+            })
+        } else {
+            res.status(200).json({
+                data
+            })
+        }
+        
+    })
 };

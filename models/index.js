8a2fc78c33d2d998b5b5ae2d714793bfe1bd1594
Steph-crown <emmanuelstephen024@gmail.com>
2021-01-15 14:55:18 +0100

    Updated mongodb uri

--- models/index.js
+++ models/index.js
@@ -1,9 +1,14 @@
 // TO CONNECT THE DATABASE
 // Mongo client
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/users', { useNewUrlParser: true })
-  .catch(err => console.log(err));
 
+// Set up mongoose connection
+var localhostUrl = 'mongodb://localhost:27017/users';
+let thisUrl = 'mongodb+srv://stephcrown:mobols2000@first-api.bg6th.mongodb.net/users?retryWrites=true&w=majority'
+var mongoDB = process.env.MONGODB_URI || localhostUrl;
+
+mongoose.connect(mongoDB, { useNewUrlParser: true })
+  .catch(err => console.log(err));
 
 
 
